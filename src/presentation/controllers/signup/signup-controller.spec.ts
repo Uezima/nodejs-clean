4cecc8a340cf586c8acef3b26b2353cbff85a7e8
Guilherme Uezima <guilherme@uezima.com>
2020-06-10 11:19:26 -0300

    refactor: improve file names

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -1,6 +1,6 @@
-import { SignUpController } from './signup'
+import { SignUpController } from './signup-controller'
 import { ServerError, MissingParamError } from '../../errors'
-import { AddAccount, AccountModel, AddAccountModel, Validation } from './signup-protocols'
+import { AddAccount, AccountModel, AddAccountModel, Validation } from './signup-controller-protocols'
 import { HttpRequest } from '../../protocols'
 import { ok, serverError, badRequest } from '../../helpers/http/http-helper'
 
