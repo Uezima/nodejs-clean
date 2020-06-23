acb56f4c9734c0f01fc65cdc09754e7570c893ef
Uezima <guilherme@uezima.com>
2020-06-23 18:51:46 -0300

    docs: add survey result api docs

---
+++ src/main/docs/paths/survey-result-path.ts
@@ -0,0 +1,47 @@
+export const surveyResultPath = {
+  put: {
+    tags: ['Survey'],
+    summary: 'Create survey answer API',
+    security: [{
+      apiKeyAuth: []
+    }],
+    requestBody: {
+      content: {
+        'application/json': {
+          schema: {
+            $ref: '#/schemas/saveSurveyParams'
+          }
+        }
+      }
+    },
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
+    responses: {
+      200: {
+        description: 'Success',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  }
+}
