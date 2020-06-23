6701c5528ec66f816c86dd032c5f3488a7f7873c
Uezima <guilherme@uezima.com>
2020-06-23 18:58:39 -0300

    refactor: move docs to separete files

---
+++ src/main/docs/schemas.ts
@@ -0,0 +1,25 @@
+import {
+  accountSchema,
+  loginParamsSchema,
+  errorSchema,
+  surveySchema,
+  surveyAnswerSchema,
+  surveysSchema,
+  signUpParamsSchema,
+  addSurveyParamsSchema,
+  saveSurveyParamsSchema,
+  surveyResultSchema
+} from './schemas/'
+
+export default {
+  account: accountSchema,
+  loginParams: loginParamsSchema,
+  signUpParams: signUpParamsSchema,
+  error: errorSchema,
+  surveys: surveysSchema,
+  survey: surveySchema,
+  surveyAnswer: surveyAnswerSchema,
+  addSurveyParams: addSurveyParamsSchema,
+  saveSurveyParams: saveSurveyParamsSchema,
+  surveyResult: surveyResultSchema
+}
