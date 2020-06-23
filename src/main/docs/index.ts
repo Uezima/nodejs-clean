23b3da31a0a01862e41f18c9bebbcc63c676dd17
Uezima <guilherme@uezima.com>
2020-06-23 14:17:23 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node API',
+    description: 'Studies about NodeJs, Typescript, Clean Architecture and SOLID',
+    version: '1.0.0'
+  }
+}
